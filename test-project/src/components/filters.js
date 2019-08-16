b6aa9665d2a9e0f9381898db4155313894bc0366
lokiron <teannew@gmail.com>
2019-08-16 05:21:25 -0500

    some changes

--- test-project/src/components/filters.js
+++ test-project/src/components/filters.js
@@ -9,28 +9,30 @@ class Filter extends Component {
             <div className="filter-logo">
                 <img alt="logo" src={logo}/>
             </div>
-            <div className="filter-content">
+            <div className="filter-content">  
                 <div className="filter-title">
                     <i className="far fa-lightbulb space"/>
                     <span>Smart Filters</span>
                 </div>
-                <r/>
+            
                 <div className="filter-items">
                     <div className="item-content">
                         <i className="fas fa-home space"/>
                         <i className="fas fa-toggle-off space-left"/>
                     </div>
                     <div className="item-content">
-                        <i className="fas fa-home space"/>
+                        <i className="fas fa-calendar-alt space"/>
                         <i className="fas fa-toggle-off space-left"/>
                     </div>
                     <div className="item-content">
-                        <i className="fas fa-home space"/>
+                        <i className="fas fa-heart space"/>
                         <i className="fas fa-toggle-off space-left"/>
                     </div>             
                 </div>
+                          
+                
                 <div className="filter-buttons">
-                    <span>Smart Filters</span>
+                    <span>Satus</span>
                     <input type="text" placeholder="Search..." />
                     <button type="button" class="btn btn-light">DELIVERED</button>
                 </div>
